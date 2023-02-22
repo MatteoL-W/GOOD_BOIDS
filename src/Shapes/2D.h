391c5ee3531bfc7ddca55ab694d793490e0c45af
Matteo Leclercq <matteo.ftt@gmail.com>
2023-02-22 12:54:34 +0100

    ✨ [Shapes] Better fish

--- src/Shapes/2D.h
+++ src/Shapes/2D.h
@@ -31,15 +31,17 @@ struct Circle {
 struct Fish {
     static void draw(p6::Context& ctx, SingleBoid boid)
     {
-        ctx.fill = {1, 1, 1, 1};
+        ctx.fill   = {1, 1, 1, 1};
+        ctx.stroke = {1, 1, 1, 1};
+        ctx.equilateral_triangle(
+            // Position in the inverse direction multiplied to be approximately 80% of the circle
+            p6::Center{boid.getPosition() - glm::vec2(boid.getRadius() * 80) * boid.getVelocity()},
+            p6::Radius{static_cast<float>(boid.getRadius())},
+            p6::Rotation{p6::Angle{boid.getVelocity()}}
+        );
         ctx.circle(
             p6::Center{boid.getPosition()},
             p6::Radius{boid.getRadius()}
-        );
-        ctx.equilateral_triangle(
-            p6::Center{boid.getPosition() - glm::vec2(1.5)*(boid.getVelocity() * glm::vec2(0.01)) },
-            p6::Radius{static_cast<float>(boid.getRadius())},
-            p6::Rotation{p6::Angle{boid.getVelocity()}}
         );
     }
 };
