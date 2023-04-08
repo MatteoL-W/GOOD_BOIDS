ef06a344262907b0255f01748f71d4ca560441a2
Matteo Leclercq <matteo.ftt@gmail.com>
2023-04-08 16:59:26 +0200

    ✨[Cone] Add the second species

--- src/Shapes/3D.cpp
+++ src/Shapes/3D.cpp
@@ -2,10 +2,16 @@
 
 namespace Shapes::ThreeDimensions {
 
-ShapesType const& get_sphere_instance()
+ShapesType const& get_sphere_instance(float radius)
 {
-    static ShapesType const sphere = Sphere{0.1f};
+    static ShapesType const sphere = Sphere{radius};
     return sphere;
+}
+
+ShapesType const& get_cone_instance(float radius)
+{
+    static ShapesType const cone = Cone{radius};
+    return cone;
 }
 
 } // namespace Shapes::ThreeDimensions
--- src/Shapes/3D/Cone.cpp
+++ src/Shapes/3D/Cone.cpp
@@ -1,19 +1,23 @@
-#include "Sphere.h"
-
+#include "Cone.h"
 #include "Utils/Mesh.h"
 
 namespace Shapes::ThreeDimensions {
 
-Sphere::Sphere(float radius)
-    : _radius(radius), _vertices(glimac::sphere_vertices(getRadius(), 32, 16)), _mesh(RenderEngine::Mesh{_vertices})
+Cone::Cone(float radius)
+    : _radius(radius), _vertices(glimac::cone_vertices(getRadius(), getRadius(), 32, 16)), _mesh(RenderEngine::Mesh{_vertices})
 {}
 
-void Sphere::draw(p6::Context& ctx, Utils::TransformAttributes const& transformAttributes) const
+void Cone::draw(p6::Context& ctx, Utils::TransformAttributes const& transformAttributes) const
 {
     _shader._program.use();
 
+    glm::vec3 up            = glm::vec3(0.f, 1.f, 0.f);
+    glm::vec3 rotationAxis  = glm::cross(up, transformAttributes._velocity);
+    float     rotationAngle = acos(glm::dot(up, transformAttributes._velocity));
+
     auto modelViewMatrix = glm::translate(glm::mat4{1}, glm::vec3(0.f, 0.f, -5.f));
     modelViewMatrix      = glm::translate(modelViewMatrix, transformAttributes._position);
+    modelViewMatrix      = glm::rotate(modelViewMatrix, rotationAngle, rotationAxis);
 
     auto projectionMatrix = glm::perspective(glm::radians(70.f), ctx.aspect_ratio(), .1f, 100.f);
     auto normalMatrix     = glm::transpose(glm::inverse(modelViewMatrix));
