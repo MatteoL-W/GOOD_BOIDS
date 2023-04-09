bfa2cbd5221804766d047f243a0487431da51108
Matteo Leclercq <matteo.ftt@gmail.com>
2023-04-09 13:35:00 +0200

    🎨[Food] Food working in 3D

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,7 @@ int main(int argc, char* argv[])
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         foodProvider.update(ctx);
-        // foodProvider.draw(ctx, testShader);
+        foodProvider.draw(ctx);
 
         boidsManager.update(obstaclesManager, foodProvider);
         boidsManager.draw(ctx);
