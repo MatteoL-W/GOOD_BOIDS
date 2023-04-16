c826149492c5c22268c45b3da1a285b6ebdad44a
Matteo Leclercq <matteo.ftt@gmail.com>
2023-04-16 13:07:20 +0200

    📝[Shaders] Remove useless comments

--- src/Rendering/Programs/Shaders/texture.fs.glsl
+++ src/Rendering/Programs/Shaders/texture.fs.glsl
@@ -1,13 +1,13 @@
 #version 330 core
 
-uniform sampler2D uTexture;// Texture
+uniform sampler2D uTexture;
 
-in vec3 vPosition_vs;// Position du sommet transformée dans l'espace View (vs)
-in vec3 vNormal_vs;// Normale du sommet transformée dans l'espace View (vs)
-in vec2 vTexCoords;// Coordonnées de texture du sommet
+in vec3 vPosition_vs;
+in vec3 vNormal_vs;
+in vec2 vTexCoords;
 
 out vec4 fFragColor;
 
 void main() {
-    fFragColor = vec4(texture(uTexture, vTexCoords).xyz, 1.);
+    fFragColor = texture(uTexture, vTexCoords);
 }
