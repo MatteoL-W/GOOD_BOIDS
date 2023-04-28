e743b103daab67adbaf43df599862572d34349a9
Matteo Leclercq _1 <matteo.ftt@gmail.com>
2023-04-28 16:52:15 +0200

    🎨[ShadyPhong] Better names (need: Light class + multiples lights)

--- src/Rendering/Programs/ShadyPhong/PhongAndShadow.h
+++ src/Rendering/Programs/ShadyPhong/PhongAndShadow.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "Rendering/Cameras/CameraManager.h"
-#include <glpp-extended/lib/glm/glm/gtc/type_ptr.hpp>
 #include <p6/p6.h>
+#include <glpp-extended/lib/glm/glm/gtc/type_ptr.hpp>
+#include "Rendering/Cameras/CameraManager.h"
 #include "utils/ProjectionMatrixHandler.h"
 
 namespace Rendering::Programs {
@@ -21,14 +21,14 @@ struct PhongAndShadow {
 
     PhongAndShadow()
         : _program{p6::load_shader("../src/Rendering/Programs/ShadyPhong/shadowRendering.vs.glsl", "../src/Rendering/Programs/ShadyPhong/shadowRendering.fs.glsl")}
-        , uDiffuseTexture(glGetUniformLocation(_program.id(), "diffuseTexture"))
-        , uShadowMap(glGetUniformLocation(_program.id(), "shadowMap"))
-        , uProjection(glGetUniformLocation(_program.id(), "projection"))
-        , uView(glGetUniformLocation(_program.id(), "view"))
-        , uModel(glGetUniformLocation(_program.id(), "model"))
-        , uLightSpaceMatrix(glGetUniformLocation(_program.id(), "lightSpaceMatrix"))
-        , uLightPos(glGetUniformLocation(_program.id(), "lightPos"))
-        , uViewPos(glGetUniformLocation(_program.id(), "viewPos"))
+        , uDiffuseTexture(glGetUniformLocation(_program.id(), "uDiffuseTexture"))
+        , uShadowMap(glGetUniformLocation(_program.id(), "uShadowMap"))
+        , uProjection(glGetUniformLocation(_program.id(), "uProjection"))
+        , uView(glGetUniformLocation(_program.id(), "uView"))
+        , uModel(glGetUniformLocation(_program.id(), "uModel"))
+        , uLightSpaceMatrix(glGetUniformLocation(_program.id(), "uLightSpaceMatrix"))
+        , uLightPos(glGetUniformLocation(_program.id(), "uLightPos"))
+        , uViewPos(glGetUniformLocation(_program.id(), "uViewPos"))
     {}
 
     void setMatrices(glm::mat4 model, glm::mat4 lightSpaceMatrix) const
--- src/Rendering/Programs/ShadyPhong/shadowRendering.vs.glsl
+++ src/Rendering/Programs/ShadyPhong/shadowRendering.vs.glsl
@@ -1,7 +1,7 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec2 aTexCoords;
+layout (location = 0) in vec3 aVertexPosition;
+layout (location = 1) in vec3 aVertexNormal;
+layout (location = 2) in vec2 aVertexTexCoords;
 
 out VS_OUT {
     vec3 FragPos;
@@ -10,16 +10,17 @@ out VS_OUT {
     vec4 FragPosLightSpace;
 } vs_out;
 
-uniform mat4 projection;
-uniform mat4 view;
-uniform mat4 model;
-uniform mat4 lightSpaceMatrix;
+uniform mat4 uProjection;
+uniform mat4 uView;
+uniform mat4 uModel;
+uniform mat4 uLightSpaceMatrix;
 
 void main()
 {
-    vs_out.FragPos = vec3(model * vec4(aPos, 1.0));
-    vs_out.Normal = transpose(inverse(mat3(model))) * aNormal;
-    vs_out.TexCoords = vec2(aTexCoords.x, 1-aTexCoords.y); // Inverse UV to have right texture (still don't know why)
-    vs_out.FragPosLightSpace = lightSpaceMatrix * vec4(vs_out.FragPos, 1.0);
-    gl_Position = projection * view * vec4(vs_out.FragPos, 1.0);
+    vs_out.FragPos           = vec3(uModel * vec4(aVertexPosition, 1.0));
+    vs_out.Normal            = transpose(inverse(mat3(uModel))) * aVertexNormal;
+    vs_out.TexCoords         = vec2(aVertexTexCoords.x, 1-aVertexTexCoords.y); // Inverse UV to have right texture (still don't know why)
+    vs_out.FragPosLightSpace = uLightSpaceMatrix * vec4(vs_out.FragPos, 1.0);
+
+    gl_Position              = uProjection * uView * vec4(vs_out.FragPos, 1.0);
 }
