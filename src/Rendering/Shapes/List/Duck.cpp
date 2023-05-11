d13c3ee2f4332b89497730bb476eba8a4493b91c
Matteo Leclercq <matteo.ftt@gmail.com>
2023-05-11 20:37:12 +0200

    ✨[Models] Rotate model according to velocity (weird bug)

--- src/Rendering/Shapes/List/Duck.cpp
+++ src/Rendering/Shapes/List/Duck.cpp
@@ -1,4 +1,5 @@
 #include "Duck.h"
+#include <glm/gtx/quaternion.hpp>
 #include "Rendering/Engine/Mesh.h"
 
 namespace Rendering::Shapes {
@@ -15,8 +16,12 @@ Duck::Duck()
 void Duck::draw(utils::TransformAttributes const& transformAttributes, utils::RenderingDatas& renderingDatas) const
 {
     glCullFace(GL_FRONT);
+
+    auto const rotationQuaternion = glm::rotation(glm::vec3(1.f, 0.f, 0.f), glm::normalize(transformAttributes._velocity));
+
     auto modelMatrix = glm::translate(glm::mat4{1}, transformAttributes._position);
-    modelMatrix      = glm::scale(modelMatrix, glm::vec3(0.01f));
+    modelMatrix      = glm::scale(modelMatrix, glm::vec3(0.005f));
+    modelMatrix      = modelMatrix * glm::mat4_cast(rotationQuaternion);
 
     switch (renderingDatas._renderType)
     {
