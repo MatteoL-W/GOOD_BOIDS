b0b86b2777c926b2b4f4fa151264d56d85a391b8
Matteo Leclercq <matteo.ftt@gmail.com>
2023-05-12 10:25:16 +0200

    ✨[Spectator] Spectator casts shadows

--- src/Spectator/Spectator.cpp
+++ src/Spectator/Spectator.cpp
@@ -7,15 +7,14 @@ Spectator::Spectator()
     : _controls(getControlsInstance())
 {}
 
-void Spectator::draw() const
+void Spectator::draw(utils::RenderingDatas& renderingDatas) const
 {
     auto transformAttributes = utils::TransformAttributes{._position = getSpectatorPosition(), ._velocity = Camera::getFront() * glm::vec3{1.f, 0.f, 1.f}};
-    auto renderingData       = utils::RenderingDatas{._renderType = utils::RenderType::Classic};
 
     std::visit(
         [&](auto const& shape) {
             if (!Camera::isFirstPerson())
-                shape.draw(transformAttributes, renderingData);
+                shape.draw(transformAttributes, renderingDatas);
         },
         _shape
     );
