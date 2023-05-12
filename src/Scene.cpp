bc6c7311e779b0bfec23034518850ac655c0b446
Matteo Leclercq <matteo.ftt@gmail.com>
2023-05-12 16:21:22 +0200

    ✨ [Lights] Better ambient

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -45,7 +45,7 @@ void Scene::initializeBoids(p6::Context& ctx)
 
 void Scene::initializeLights()
 {
-    _renderingDatas._directional = Rendering::Lights::Directional{{.0f, 5.0f, -5.f}, {0.f, 0.f, 0.f}, .01f, .2f, .3f};
+    _renderingDatas._directional = Rendering::Lights::Directional{{.0f, 5.0f, -5.f}, {0.f, 0.f, 0.f}, .1f, .2f, .3f};
     _renderingDatas._points      = {
         Rendering::Lights::Point{Spectator::getSpectatorPosition(), 1.f, .09f, .032f, .01f, .3f, .4f},
     };
