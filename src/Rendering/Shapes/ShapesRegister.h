eeea1249ccbb4bf849f5dade203e65086eeac512
Matteo Leclercq <matteo.ftt@gmail.com>
2023-05-24 14:49:33 +0200

    ✨ [Food] Star shape (it's black?)

--- src/Rendering/Shapes/ShapesRegister.h
+++ src/Rendering/Shapes/ShapesRegister.h
@@ -3,12 +3,12 @@
 #include "Rendering/Shapes/List/Cloud.h"
 #include "Rendering/Shapes/List/Drop.h"
 #include "Rendering/Shapes/List/Sheep.h"
-#include "Rendering/Shapes/List/Sphere.h"
+#include "Rendering/Shapes/List/Star.h"
 
 using BoidsShapesType     = std::variant<Rendering::Shapes::Cloud>;
 using SpectatorShapesType = std::variant<Rendering::Shapes::Sheep>;
 using ObstaclesShapesType = std::variant<Rendering::Shapes::Drop>;
-using FoodShapesType      = std::variant<Rendering::Shapes::Sphere>;
+using FoodShapesType      = std::variant<Rendering::Shapes::Star>;
 
 namespace Rendering::Shapes {
 
